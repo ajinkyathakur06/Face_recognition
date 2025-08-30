5ee034f592839e6bafe2484b65dec3b3178aeb52
Ajinkyaathakur <ajinkyaathakur@gmail.com>
2025-08-30 18:47:21 +0530

    Database settings changed

--- Attendence_System/settings.py
+++ Attendence_System/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'Attendence_System.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': '/data/db.sqlite3',  # ✅ points to PVC-mounted directory
     }
 }
 
